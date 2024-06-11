3612ff533e8bc01774a1d6f3cd06d4e54036a30e
moseshansen <moseshansen0@gmail.com>
2024-06-11 11:52:05 -0600

    Add evaluation callback

--- ppo_test.py
+++ ppo_test.py
@@ -6,12 +6,15 @@ from sb3_contrib.common.maskable.evaluation import evaluate_policy
 
 from wrapped_env import CTFENVMA_sb3
 
-import numpy as np
-
 # Initialize environment with 1 mobile per team, and reward of 1 for winning
 env = CTFENVMA_sb3(1,1,0)
-env = ActionMasker(env, "action_mask")
+
+def get_mask(env):
+    return env.action_mask()
+
+env = ActionMasker(env, get_mask)
 
 # Initialize maskable PPO model
 model = MaskablePPO(MaskableActorCriticPolicy, env, seed=42, verbose=1)
-model.learn(total_timesteps=1e9, callback=MaskableEvalCallback, progress_bar=True)
+eval_callback = MaskableEvalCallback(env, n_eval_episodes=10, eval_freq=10_000, log_path="./ppo_evaluations", use_masking=True) # Note: you will need to create a folder for the evaluations to be logged in
+model.learn(total_timesteps=1e6, callback=eval_callback, progress_bar=True)
